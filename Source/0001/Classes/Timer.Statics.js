70b3197101b5591f747a52da8441dcdd590d0237
E.P. de Haas <serioushare@gmail.com>
2017-08-31 18:58:00 +0200

    Create Timer.Statics.js

---
+++ Source/0001/Classes/Timer.Statics.js
@@ -0,0 +1,14 @@
+
+Object.defineProperty(Timer, "PRECISION",             {value: 1,                        enumerable:true});
+
+Object.defineProperty(Timer, "STOPPED",               {value: 0,                        enumerable:true});
+Object.defineProperty(Timer, "PLAYING",               {value: 1,                        enumerable:true});
+
+Object.defineProperty(Timer, "DEFAULT_TIME",          {value: 0,                        enumerable:true});
+Object.defineProperty(Timer, "DEFAULT_INTERVAL",      {value: 1000,                     enumerable:true});
+Object.defineProperty(Timer, "DEFAULT_PLAYSTATE",     {value: Timer.STOPPED,            enumerable:true});
+Object.defineProperty(Timer, "DEFAULT_SPEED",         {value: 1,                        enumerable:true});
+Object.defineProperty(Timer, "DEFAULT_BOTTOM",        {value: Number.NEGATIVE_INFINITY, enumerable:true});
+Object.defineProperty(Timer, "DEFAULT_TOP",           {value: Number.POSITIVE_INFINITY, enumerable:true});
+Object.defineProperty(Timer, "DEFAULT_LOOP",          {value: false,                    enumerable:true});
+Object.defineProperty(Timer, "DEFAULT_RAISES_EVENTS", {value: true,                     enumerable:true});
