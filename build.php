334f3083aba5a988505040d69cca5bd0efff2394
E.P. de Haas <serioushare@gmail.com>
2017-08-31 22:28:04 +0200

    Update build.php

--- build.php
+++ build.php
@@ -2,12 +2,20 @@
 header('Content-Type: application/javascript');
 
 $build = $_GET["b"]? in_array($_GET["b"], scandir("Source"))? $_GET["b"] : array_pop(scandir("Source")) : array_pop(scandir("Source"));
+
+$manifest_raw = file_get_contents("Source/$build/manifest.json");
+$manifest = json_decode($manifest_raw);
+
+$version  = $manifest->version->major.".";
+$version .= $manifest->version->minor.".";
+$version .= $manifest->version->build < 10000? substr("000".$manifest->version->build, -4) : $manifest->version->build;
+
 ?>
 
-/**[SHTimer]**********************************************************************************
- * Name:       SeriousTimer                                                                  *
- * Version:    0.1.<?=$build?>                                                                      *
- * Language:   Javascript                                                                    *
+/**[SeriousBuilder]***************************************************************************
+ * Name:       <?=$manifest->project?> 
+ * Version:    <?=$version?> 
+ * Language:   JavaScript 
  *****************************************************************************[Serious Hare]**/
 
 <?
