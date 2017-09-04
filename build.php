56f7c73218204e14c2b293f9b77c8e3cbd1337ce
E.P. de Haas <serioushare@gmail.com>
2017-09-04 16:04:53 +0200

    Update build.php

--- build.php
+++ build.php
@@ -3,26 +3,31 @@ header('Content-Type: application/javascript');
 
 $build = $_GET["b"]? in_array($_GET["b"], scandir("Source"))? $_GET["b"] : array_pop(scandir("Source")) : array_pop(scandir("Source"));
 
-$manifest_raw = file_get_contents("Source/$build/manifest.json");
-$manifest = json_decode($manifest_raw);
+$build_raw = file_get_contents("Source/$build/build.json");
+$buildplan = json_decode($build_raw);
 
-$version  = $manifest->version->major.".";
-$version .= $manifest->version->minor.".";
-$version .= $manifest->version->build < 10000? substr("000".$manifest->version->build, -4) : $manifest->version->build;
+$version  = $buildplan->version->major.".";
+$version .= $buildplan->version->minor.".";
+$version .= $buildplan->version->build < 10000? substr("000".$buildplan->version->build, -4) : $buildplan->version->build;
 
 ?>
 
 /**[SeriousBuilder]***************************************************************************
- * Name:       <?=$manifest->project?> 
+ * Name:       <?=$buildplan->project?> 
  * Version:    <?=$version?> 
  * Language:   JavaScript 
  *****************************************************************************[Serious Hare]**/
 
 <?
-include("Source/$build/Classes/Timer.js");
-include("Source/$build/Classes/Timer.Statics.js");
-include("Source/$build/Classes/TimerEvent.js");
-include("Source/$build/Classes/TimerMarker.js");
 
-include("Source/$build/Extends/Math.js");
+if($buildplan->use_closure)echo "(function closure(context){\n";
+
+
+for($i=0; $i<count($buildplan->files); $i++){
+	$file = $buildplan->files[$i]->path;
+	include("Source/$build/$file");
+}
+
+if($buildplan->use_closure)echo "}(this))";
+
 ?>
