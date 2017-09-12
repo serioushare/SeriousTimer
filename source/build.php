e65a68e1dd58c2fe70cfacc79ccbd38d435f91b7
E.P. de Haas <serioushare@gmail.com>
2017-09-12 22:58:24 +0200

    Update build.php

--- source/build.php
+++ source/build.php
@@ -1,14 +1,15 @@
+
+/**[SeriousBuilder]***************************************************************************
 <?
 header('Content-Type: application/javascript');
-$build = $_GET["b"]? in_array($_GET["b"], scandir("../source"))? $_GET["b"] : array_pop(scandir("../source")) : array_pop(scandir("../source"));
+$builds = array_diff(scandir("../source"), array(".", "..", "build.php"));
+$build = $_GET["b"]? in_array($_GET["b"], $builds)? $_GET["b"] : array_pop($builds) : array_pop($builds);
 $build_raw = file_get_contents("$build/build.json");
 $buildplan = json_decode($build_raw);
 $version  = $buildplan->version->major.".";
 $version .= $buildplan->version->minor.".";
 $version .= $buildplan->version->build < 10000? substr("000".$buildplan->version->build, -4) : $buildplan->version->build;
 ?>
-
-/**[SeriousBuilder]***************************************************************************
  * Name:       <?=$buildplan->project?> 
  * Version:    <?=$version?> 
  * Language:   JavaScript 
